243eb298a5c81fe1feb239ba0a0ba5408d41be88
Ali Gohar <aligoharkp@gmail.com>
2024-09-15 22:05:23 +0500

    done with multiple routes

--- server.mjs
+++ server.mjs
@@ -2,7 +2,22 @@ import {createServer} from 'node:http'
 
 const PORT = 3000
 const server = createServer((req,res)=>{
-    res.end('Server is listening');
+    // res.end('Server is listening');
+
+    const url = req.url;
+    if (url === '/') {
+        res.end('Welcome to Home Page.')
+    }
+    else if (url === '/about') {
+        res.end('This is About Page.')
+    }
+    else if (url === '/contact') {
+        res.end('This is Contact Page.')
+    }
+    else{
+        res.statusCode = 404
+        res.end('Page Not Found')
+    }
 })
 
 server.listen(PORT,()=>{
